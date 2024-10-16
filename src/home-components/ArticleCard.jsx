7357d1df82ea33a59141057d319356f16e55828a
TheStringKing <dandreopperman@gmail.com>
2024-10-16 15:33:10 -0400

    add css styling changes

--- src/home-components/ArticleCard.jsx
+++ src/home-components/ArticleCard.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FaThumbsUp } from "react-icons/fa";
+import { FaComments } from "react-icons/fa";
 export function ArticleCard({ article }) {
   return (
     <div className="card">
@@ -11,8 +13,12 @@ export function ArticleCard({ article }) {
       </Link>
       <img className="card-img" src={article.article_img_url} />
       <div className="card-info">
-        <p className="card-votes">votes: {article.votes}</p>
-        <p className="card-comments">comments: {article.comment_count}</p>
+        <p className="card-votes">
+          <FaThumbsUp className="icon" /> {article.votes}
+        </p>
+        <p className="card-comments">
+          <FaComments className="icon" /> {article.comment_count}
+        </p>
       </div>
     </div>
   );
